f78958e19eb14e87b68007bd144b078e64f92f5c
Bill Tran <bill@billjohntran.com>
2017-08-20 22:48:52 -0700

    refactored component using styled-component

--- src/components/SalesByMonthCard.js
+++ src/components/SalesByMonthCard.js
@@ -1,100 +1,130 @@
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types'; // eslint-disable-line
 import graph from '../assets/graph.png';
 import sparkLine from '../assets/spark_line.png';
+import pencil from '../assets/pencil_icons.png';
+
+const Container = styled.div`
+  display: inline-block;
+  width: 242px;
+  padding: 18px 0px 0px;
+  background: white;
+  margin: 24px 20px 0px 0px;
+  vertical-align: top;
+  border: 1px solid #cdccbc;
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const Title = styled.div`
+  padding: 0 20px;
+`;
+
+const PencilBox = styled.div`
+  float: right;
+  width: 20px;
+  height: 20px;
+  border: 1px solid rgba(128, 128, 128, 0.24);
+  border-radius: 4px;
+  padding: 2px 0px 0px 2px;
+`;
+
+const Pencil = styled.div`
+  background-image: url(${pencil});
+  width: 20px;
+  height: 15px;
+
+  &:hover {
+    background-position: 0 -17px;
+  }
+`;
+
+const SaleByMonth = styled.p`
+  font-size: 10px;
+  margin: 15px 0 0 0;
+  padding: 0 20px;
+`;
+
+const GraphImg = styled.img`
+  padding: 0 20px;
+  margin-bottom: 4px;
+`;
+
+const CurrentMonthlyContainer = styled.div`
+  padding: 0 20px;
+  background: #fbfbf6;
+  border-top: 1px solid rgb(246, 246, 238);
+  border-bottom: 1px solid rgb(246, 246, 238);
+  color: rgb(181, 181, 170);
+`;
+
+const CurrentMonthlyTextArea = styled.div`
+  padding: 4px 0px;
+  display: inline-block;
+  margin-right: 34px;
+
+  & p:first-child {
+    font-size: 8px;
+    margin: 8px 0 1px;
+  }
+
+  & p:last-child {
+    font-size: 10px;
+    margin: 0;
+  }
+`;
+
+const CurrentMonthlyCenter = CurrentMonthlyTextArea.extend`
+  margin-right: 16px;
+`;
+
+const CurrentMonthlyGraphArea = styled.div`
+  padding: 2px 0px;
+  display: inline-block;
+  vertical-align: top;
+
+  & p {
+    font-size: 8px;
+    margin: 10px 0px -2px;
+    text-align: center;
+  }
+`;
+
+const More = styled.p`
+  font-size: 10px;
+  margin: 5px 20px;
+`;
 
 const SalesByMonthCard = ({ name }) => {
   return (
-    <div style={{
-      display: 'inline-block',
-      width: '242px',
-      padding: '18px 0px 0px',
-      background: 'white',
-      margin: '24px 20px 0px 0px',
-      verticalAlign: 'top',
-      border: '1px solid #cdccbc',
-      borderRadius: '4px',
-      overflow: 'hidden',
-    }}
-    >
-      <div style={{
-        padding: '0 20px',
-      }}>
+    <Container>
+      <Title>
         <span>{name}</span>
-        <div style={{
-          float: 'right',
-          width: '20px',
-          height: '20px',
-          border: '1px solid rgba(128, 128, 128, 0.24)',
-          borderRadius: '4px',
-          padding: '2px 0px 0px 2px',
-        }}>
-          <div className='pencil'></div>
-        </div>
-      </div>
-      <p style={{
-        fontSize: '10px',
-        margin: '15px 0 0 0',
-        padding: '0 20px',
-      }}>Sales by month</p>
-      <img style={{
-        padding: '0 20px',
-        marginBottom: '4px',
-      }} src={graph} />
-      <div style={{
-        padding: '0 20px',
-        background: '#fbfbf6',
-        borderTop: '1px solid rgb(246, 246, 238)',
-        borderBottom: '1px solid rgb(246, 246, 238)',
-        color: 'rgb(181, 181, 170)',
-      }}>
-        <div style={{
-          padding: '4px 0px',
-          display: 'inline-block',
-          marginRight: '34px',
-        }}>
-          <p style={{
-            fontSize: '8px',
-            margin: '8px 0 1px',
-          }}>Total Monthly</p>
-          <p style={{
-            fontSize: '10px',
-            margin: '0',
-          }}>Sales</p>
-        </div>
-        <div style={{
-          padding: '4px 0px',
-          display: 'inline-block',
-          marginRight: '16px',
-        }}>
-          <p style={{
-            fontSize: '8px',
-            margin: '8px 0 1px',
-          }}>Current</p>
-          <p style={{
-            fontSize: '10px',
-            margin: '0',
-          }}>$23,483</p>
-        </div>
-        <div style={{
-          padding: '2px 0px',
-          display: 'inline-block',
-          verticalAlign: 'top',
-        }}>
-          <p style={{
-            fontSize: '8px',
-            margin: '8px 0px -1px',
-          }}>Total Monthly</p>
-          <img src={sparkLine} />
-        </div>
-        <p style={{
-          fontSize: '10px',
-          margin: '5px -2px',
-        }}>more</p>
-      </div>
-    </div>
-  )
-}
+        <PencilBox>
+          <Pencil />
+        </PencilBox>
+      </Title>
+      <SaleByMonth>Sales by month</SaleByMonth>
+      <GraphImg src={graph} />
+      <CurrentMonthlyContainer>
+        <CurrentMonthlyTextArea>
+          <p>Total Monthly</p>
+          <p>Sales</p>
+        </CurrentMonthlyTextArea>
+        <CurrentMonthlyCenter>
+          <p>Current</p>
+          <p>$23,483</p>
+        </CurrentMonthlyCenter>
+        <CurrentMonthlyGraphArea>
+          <p>1 - Year</p>
+          <img src={sparkLine} alt="Graph Line" />
+        </CurrentMonthlyGraphArea>
+      </CurrentMonthlyContainer>
+      <More>more</More>
+    </Container>
+  );
+};
 
 SalesByMonthCard.propTypes = {
   name: PropTypes.string.isRequired,
