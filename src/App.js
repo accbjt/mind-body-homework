c0c2f8299dc2079b8d5ba4310301e944be695121
Bill Tran <bill@billjohntran.com>
2017-08-23 00:52:52 -0700

    refactored App.js to use styled-component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import SalesByMonthCardContainer from './containers/SalesByMonthCardContainer'
 import NewFormContainer from './containers/NewFormContainer'
 import OverviewSection from './components/OverviewSection'
@@ -8,125 +9,162 @@ import logo from './assets/logo.png';
 import plus from './assets/plus_icon.png';
 import graph from './assets/graph.png';
 import sparkLine from './assets/spark_line.png';
-import './App.css';
 
-const style = {
-  dashboard: {
-    position: 'relative',
-    top: '-1px',
-    display: 'inline-block',
-    padding: '10px 32px',
-    fontSize: '13px',
-    fontWeight: 'bolder',
-    letterSpacing: '0.73px',
-    border: 'solid 1px #c9c9c9',
-    borderBottom: '1px solid #ffffff',
-    borderRadius: '3px 3px 0px 0px',
-    transform: 'scaleY(1.10)',
-    backgroundColor: 'white',
-  },
-  li: {
-    display: 'inline-block',
-    padding: '11px 24px',
-    fontSize: '13px',
-    fontWeight: 'bolder',
-    letterSpacing: '0.73px',
-    color: 'rgb(125, 158, 141)',
-  },
-  nav: {
-    li: {
-      padding: '15px',
-      display: 'inline-block',
-      fontSize: '15px',
-    },
-  },
-};
+const AppContainer = styled.div`
+  background: #ecf7f2;
+  position: absolute;
+  top: 0px;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Logo = styled.img`
+  position: absolute;
+  top: 10px;
+  left: 14px;
+`;
+
+const TopBar = styled.div`
+  position: relative;
+  float: right;
+`;
+
+const TopBarUl = styled.ul`
+  margin: 25px 0px -1px 0px;
+  padding: 0;
+  border-top: 1px solid rgb(220, 231, 234);
+  border-bottom: 1px solid #c9c9c9;
+  background: linear-gradient(to bottom, white 50%, #ebf0ee 100%);
+`;
+
+const DashboardLi = styled.li`
+  position: relative;
+  top: -1px;
+  display: inline-block;
+  padding: 10px 32px;
+  font-size: 13px;
+  font-weight: bolder;
+  letter-spacing: 0.73px;
+  border: solid 1px #c9c9c9;
+  border-bottom: 1px solid #ffffff;
+  border-radius: 3px 3px 0px 0px;
+  transform: scaleY(1.10);
+  background-color: white;
+`;
+
+const TopBarLi = styled.li`
+  display: inline-block;
+  padding: 11px 24px;
+  font-size: 13px;
+  font-weight: bolder;
+  letter-spacing: 0.73px;
+  color: rgb(125, 158, 141);
+`;
+
+const TopBarEndingLi = TopBarLi.extend`
+  margin-right: '10px'
+`;
+
+const NewProgramButtonContainer = styled.div`
+  margin: 0 57px 0 87px;
+  padding-top: 25px;
+  display: inline-block;
+`
+
+const NewProgramButton = styled.div`
+  display: inline-block;
+  padding: 18px 22px 13px 23px;
+  border: 1px solid #9cbc88;
+  border-radius: 5px;
+  background-image: linear-gradient(#bbd69f 0%, #9cc087 100%);
+  box-shadow: inset 0px 1px 0px 0px #d6f3ba;
+`
+
+const NewProgramText = styled.p`
+  font-size: 11px;
+  text-align: center;
+  margin-top: 3px;
+`;
+
+const ProgramSection = styled.div`
+  background: linear-gradient(to right, #dfdfd5 0%,#f4f4ee 10%, #f4f4ee 90%,#dfdfd5 100%);
+`;
+
+const CardSectionContainer = styled.div`
+  display: inline-block;
+  vertical-align: top;
+  padding: 0 10px;
+  width: 70%;
+`;
+
+const ServiceSection = styled.div`
+  clear: right;
+  padding-left: 35px;
+  background-color: #ffffff;
+  border-top: 1px solid rgb(220, 231, 234);
+  border-bottom: 1px solid #c9c9c9;
+
+  & ul {
+    margin: 0;
+  }
+`;
+
+const ServiceSectionLi = styled.li`
+  display: inline-block;
+  padding: 11px 24px;
+  font-size: 13px;
+  font-weight: bolder;
+  letter-spacing: 0.73px;
+  color: rgb(125, 158, 141);
+`;
+
+const ServiceSectionliBold = ServiceSectionLi.extend`
+  font-weight: bolder;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{
-        background: '#ecf7f2',
-        position: 'absolute',
-        top: '0px',
-        width: '100vw',
-        height: '100vh'
-      }}>
-        <div className="App-header">
-          <img src={logo} style={{position: 'absolute', top: '10px', left: '14px'}}/>
+      <AppContainer>
+        <div>
+          <Logo src={logo} />
         </div>
         <div>
-          <div style={{
-            position: 'relative',
-            float: 'right',
-          }}>
-            <ul style={{
-              margin: '25px 0px -1px 0px',
-              padding: '0',
-              borderTop: '1px solid rgb(220, 231, 234)',
-              borderBottom: '1px solid #c9c9c9',
-              background: 'linear-gradient(to bottom, white 50%, #ebf0ee 100%)',
-            }}>
-              <li style={style.dashboard}>Dashboard</li>
-              <li style={style.li}>Sign In</li>
-              <li style={style.li}>Classes</li>
-              <li style={style.li}>Workshops</li>
-              <li style={style.li}>Appointments</li>
-              <li style={style.li}>Client Home</li>
-              <li style={Object.assign({}, style.li, {marginRight: '10px'})}>Retail</li>
-            </ul>
-          </div>
+          <TopBar>
+            <TopBarUl>
+              <DashboardLi>Dashboard</DashboardLi>
+              <TopBarLi>Sign In</TopBarLi>
+              <TopBarLi>Classes</TopBarLi>
+              <TopBarLi>Workshops</TopBarLi>
+              <TopBarLi>Appointments</TopBarLi>
+              <TopBarLi>Client Home</TopBarLi>
+              <TopBarEndingLi>Retail</TopBarEndingLi>
+            </TopBarUl>
+          </TopBar>
         </div>
-        <div style={{
-          clear: 'right',
-          paddingLeft: '35px',
-          backgroundColor: '#ffffff',
-          borderTop: '1px solid rgb(220, 231, 234)',
-          borderBottom: '1px solid #c9c9c9',
-        }}>
-          <ul style={{margin: '0'}}>
-            <li style={style.nav.li}>Dashboard</li>
-            <li style={Object.assign({}, style.nav.li, {fontWeight: 'bolder'})}>
-              Bussiness Overview
-            </li>
-            <li style={style.nav.li}>Schedule</li>
-            <li style={style.nav.li}>Reports</li>
+        <ServiceSection>
+          <ul>
+            <ServiceSectionLi>Dashboard</ServiceSectionLi>
+            <ServiceSectionliBold>Bussiness Overview</ServiceSectionliBold>
+            <ServiceSectionLi>Schedule</ServiceSectionLi>
+            <ServiceSectionLi>Reports</ServiceSectionLi>
           </ul>
-        </div>
-        <div style={{
-          background: 'linear-gradient(to right, #dfdfd5 0%,#f4f4ee 10%, #f4f4ee 90%,#dfdfd5 100%)',
-        }}>
-        <div style={{margin: '0 57px 0 87px', paddingTop: '25px', display: 'inline-block'}}>
-          <div style={{
-            display: 'inline-block',
-            padding: '18px 22px 13px 23px',
-            border: '1px solid #9cbc88',
-            borderRadius: '5px',
-            backgroundImage: 'linear-gradient(#bbd69f 0%, #9cc087 100%)',
-            boxShadow: 'inset 0px 1px 0px 0px #d6f3ba',
-          }}>
-          <img src={plus} />
-          </div>
-            <p style={{
-              fontSize: '11px',
-              textAlign: 'center',
-              marginTop: '3px',
-            }}>New Program</p>
-          </div>
+        </ServiceSection>
+        <ProgramSection>
+          <NewProgramButtonContainer>
+            <NewProgramButton>
+              <img src={plus} />
+            </NewProgramButton>
+            <NewProgramText>New Program</NewProgramText>
+          </NewProgramButtonContainer>
 
-          <div style={{
-            display: 'inline-block',
-            verticalAlign: 'top',
-            padding: '0 10px',
-            width: '70%',
-          }}>
-
+          <CardSectionContainer>
             <NewFormContainer />
             <SalesByMonthCardContainer />
             <OverviewSection />
-          </div>
-        </div>
-      </div>
+          </CardSectionContainer>
+        </ProgramSection>
+      </AppContainer>
     );
   }
 }
